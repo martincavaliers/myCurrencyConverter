0cb853d8a1b4a2df8575de7dcbbd412318c87bae
martincavaliers <martincoutts5@gmail.com>
2018-09-20 14:28:48 +0100

    issues parsing the return from the api

--- app.js
+++ app.js
@@ -1,59 +1,25 @@
-'use strict';
+'use strict'
 
-let currentCurrency;
-let currentAmount;
-let newCurrency;
-let transfer;
-let transferRate;
-let newValue;
+var currencyJson;
+var currencyType;
 
-// Breaks down object nesting to access 
-const getNestedObject = (nestedObj, pathArr) => {
-    return pathArr.reduce((obj, key) =>
-        (obj && obj[key] !== 'undefined') ? obj[key] : undefined, nestedObj);
-}
-
-// Gets current transfer rate from API
-function $getTransferRate(currency1, currency2){
-    const transferSelector = currency1+"_"+currency2;
-    // console.log(transferSelector);
-    $.get( "https://free.currencyconverterapi.com/api/v6/convert?q="+transferSelector , function( data ) {
-         transfer = getNestedObject(data.results, [transferSelector, 'val']);
-        //  console.log(transfer);
+function getList(){
+    fetch('https://free.currencyconverterapi.com/api/v6/currencies')
+    .then(function(response) {
+        return response.json();
+      })
+      .then(function(myJson) {
+        JSON.stringify(myJson);
+        currencyJson = myJson.results;
       });
 }
 
-function $getVal(selector){
-     return $(selector).val();
+function parseList(arr){
+    for(var i = 0; i < arr.length; i++){
+        console.log(arr[i]);
+    }
 }
 
-function calculateValue(a, b){
-    let newAmount = a * b;
-    let newVal = toString(newAmount);
-    return newVal;
-}
-
-// Document ready wrapper
 $(document).ready(function(){
-    
-    // Calculate button event handler
-    $('#calculateBtn').click(function(){
-
-        
-        currentCurrency = $getVal('#currentCurrencyType');
-        newCurrency = $getVal('#newCurrencyType');
-
-        // Get current amount
-        currentAmount = $getVal('#currentMoney');
-
-        // Get transfer rate
-         $getTransferRate(currentCurrency, newCurrency);
-
-         $('#transferRate').val(transfer);
-
-         calculateValue(currentAmount, transfer);
-
-         $('#newValue').val(newValue);
-    });
-
+     getList();
 });
