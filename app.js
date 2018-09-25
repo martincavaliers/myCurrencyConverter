fe6fe9cf7f42fb0911c2abfc0037a54372ddff88
martincavaliers <martincoutts5@gmail.com>
2018-09-25 10:50:22 +0100

    Api data populating selects correctly

--- app.js
+++ app.js
@@ -17,19 +17,19 @@ function createArray(api, array) {
         $.each(data.results, function (index, currency) {
             currencyJson.push(currency);
             // console.log("currency: ", currency);
+        });
 
-            // Populate selects
-            $.each(currencyJson, function (index, currency) {
-                var symbol = currency.id;
-                $('#currentCurrencyType').append(`<option value = ${symbol}>` + currency.currencyName + '</option>');
-                // console.log(currency.currencyName);;
-            });
+        // Populate selects
+        $.each(currencyJson, function (index, currency) {
+            var symbol = currency.id;
+            $('#currentCurrencyType').append(`<option value = ${symbol}>` + currency.currencyName + '</option>');
+            // console.log(currency.currencyName);;
+        });
 
-            $.each(currencyJson, function (index, currency) {
-                var symbol = currency.id;
-                $('#newCurrencyType').append(`<option value = ${symbol}>` + currency.currencyName + '</option>');
-                // console.log(currency.currencyName);;
-            });
+        $.each(currencyJson, function (index, currency) {
+            var symbol = currency.id;
+            $('#newCurrencyType').append(`<option value = ${symbol}>` + currency.currencyName + '</option>');
+            // console.log(currency.currencyName);;
         });
     });
 }
@@ -43,3 +43,9 @@ function populateSelect(selectID) {
 $(document).ready(function () {
     createArray("https://free.currencyconverterapi.com/api/v6/currencies", currencyJson);
 });
+
+// for(var i = 0; i < currencyJson.length; i++){
+//     var symbol = currencyJson[i].id;
+//     var currency = currencyJson[i].currencyName
+//     $('#currentCurrencyType').append(`<option value = ${symbol}>` + currency + '</option>')
+// }
